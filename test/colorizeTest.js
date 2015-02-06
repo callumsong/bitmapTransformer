755032588dab7e881c3bffe38ea286af50ea1eab
callumsong <ghksong@gmail.com>
2015-02-06 12:48:54 -0800

    tests are finally working hoorayyy

--- test/colorizeTest.js
+++ test/colorizeTest.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var expect = require("chai").expect;
 var colorize = require("../lib/colorize");
-var bitmapColorize = fs.readFileSync('colorize.bmp');
 
 describe("creating colorized bitmap", function() {
   it("should be a buffer", function() {
--- test/randomizeTest.js
+++ test/randomizeTest.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var expect = require("chai").expect;
 var randomize = require("../lib/randomize");
-var bitmapRandomize = fs.readFileSync('randomize.bmp');
 
 describe("creating randomized bitmap", function() {
   it("should be a buffer", function() {
